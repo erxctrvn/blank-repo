ff61ad59feba35000bd1b86cb90c606db19f2784
Khoa Tran <22076015@student.westernsydney.edu.au>
2026-08-12 10:31:17 +1000

    Add application demonstration

---
+++ app.mjs
@@ -0,0 +1,9 @@
+import {
+    capitalise,
+    reverse,
+    isPalindrome
+} from './string-toolbox.mjs';
+
+console.log(capitalise('hello world'));
+console.log(reverse('hello'));
+console.log(isPalindrome('racecar'));
